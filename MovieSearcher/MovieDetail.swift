f62d1db525a9b49198aa7ab2ece015b7f7c77cb2
David Maiman <david.maiman@gmail.com>
2022-07-31 17:18:46 -0400

    Added movie model details and keys

--- MovieSearcher/MovieDetail.swift
+++ MovieSearcher/MovieDetail.swift
@@ -9,4 +9,18 @@ import Foundation
 
 struct MovieDetail : Codable {
     
+    let title: String
+    let year: String
+    let rated: String
+    let runtime: String
+    let plot: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case rated = "Rated"
+        case runtime = "Runtime"
+        case plot = "Plot"
+    }
+    
 }
