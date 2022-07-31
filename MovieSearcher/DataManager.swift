d72c669e5508ff82bf6df3dd0a524700952c545c
David Maiman <david.maiman@gmail.com>
2022-07-31 17:06:12 -0400

    Add new movie model and datamanager

---
+++ MovieSearcher/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  MovieSearcher
+//
+//  Created by David on 7/31/22.
+//
+
+import Foundation
---
+++ MovieSearcher/MovieDetail.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDetail.swift
+//  MovieSearcher
+//
+//  Created by David on 7/31/22.
+//
+
+import Foundation
+
+struct MovieDetail : Codable {
+    
+}
