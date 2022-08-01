c0b7b0c1afc33d2d58abb6d599bd9427638521ed
David Maiman <david.maiman@gmail.com>
2022-07-31 22:55:15 -0400

    Added title to top of screen

--- MovieSearcher/View/MovieDetailViewController.swift
+++ MovieSearcher/View/MovieDetailViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     var viewModel: MovieDetailViewModel?
+    @IBOutlet weak var lblTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lblTitle.text = viewModel?.details?.title
         print(viewModel?.details?.title ?? "None")
     }
 }
