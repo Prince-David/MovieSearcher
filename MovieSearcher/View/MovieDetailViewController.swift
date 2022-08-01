e9276564969d20c795b5c6d08e65fba5bbb5e716
David Maiman <david.maiman@gmail.com>
2022-08-01 09:35:37 -0400

    Load image from URL

--- MovieSearcher/View/MovieDetailViewController.swift
+++ MovieSearcher/View/MovieDetailViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 class MovieDetailViewController: UIViewController {
     var viewModel: MovieDetailViewModel?
     @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var imgPoster: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lblTitle.text = viewModel?.details?.title
         print(viewModel?.details?.title ?? "None")
+        
+        guard let posterURL = viewModel?.details?.poster else {
+            return
+        }
+        
+        guard let url = URL(string: posterURL) else {
+            return
+        }
+        if let data = try? Data(contentsOf: url) {
+            imgPoster.image = UIImage(data: data)
+        }
     }
 }
