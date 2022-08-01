f64d276656a012997e765f4ca0f920dcd755a86f
David Maiman <david.maiman@gmail.com>
2022-08-01 08:35:54 -0400

    Show plot

--- MovieSearcher/View/MovieDetailViewController.swift
+++ MovieSearcher/View/MovieDetailViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class MovieDetailViewController: UIViewController {
     var viewModel: MovieDetailViewModel?
     @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSynopsis: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         lblTitle.text = viewModel?.details?.title
+        lblSynopsis.text = viewModel?.details?.plot
         print(viewModel?.details?.title ?? "None")
     }
 }
